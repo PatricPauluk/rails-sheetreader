c60f83c0e28aca2c03da1e5f914ca3e3e51dfc8f
Patric Pauluk <patric.pauluk@outlook.com>
2025-09-16 22:39:57 -0300

    Adding simpler tests to improve code coverage and ensure basic functionality.

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -10,4 +10,14 @@ RSpec.describe Product, type: :model do
     product = Product.new(name: nil, price: 9.99, quantity: 10)
     expect(product).to_not be_valid
   end
+
+  it 'é inválido sem um preço' do
+    product = Product.new(name: 'Test Product', price: nil, quantity: 10)
+    expect(product).to_not be_valid
+  end
+
+  it 'é inválido sem uma quantidade' do
+    product = Product.new(name: 'Test Product', price: 9.99, quantity: nil)
+    expect(product).to_not be_valid
+  end
 end
